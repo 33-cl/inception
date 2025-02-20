23fcc13f8682c3aeb43d945c4a43c811fe498c45
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-02-20 19:20:18 +0100

    changing conf

--- srcs/requirements/mariadb/init_db.sh
+++ srcs/requirements/mariadb/init_db.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Démarrer MySQL
-service mysql start
+# Launch MySQL
+service mysql start;
 
-# Créer la base de données
+# Create table
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 
-# Créer l'utilisateur et lui donner les droits
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
+# Create user
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
-# Modifier le mot de passe root
+# Change root password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 
-# Rafraîchir les privilèges
+# Reset privileges
 mysql -e "FLUSH PRIVILEGES;"
 
-# Arrêter MySQL proprement
+# Restart MySQL
 mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-
-# Lancer MySQL en mode sécurisé
 exec mysqld_safe
